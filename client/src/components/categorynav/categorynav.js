40d6f8ab5dbf6aaf8e69df285f9471cbca753932
Sureshbar <15122002suresh@gmail.com>
2023-03-18 18:17:11 +0530

    Sureshbar/sidebar.js
    updated sidebar

--- client/src/components/categorynav/categorynav.js
+++ client/src/components/categorynav/categorynav.js
@@ -5,31 +5,21 @@ import Sidebar from './sidebar';
 
 const categorynav = () => {
   return (
-<div className='d-flex flex-grow-1'>
-
-
-
-<div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-  <div className="offcanvas-header">
-    <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel"><Sidebar/></h5>
-    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  
-</div>
-
-
+<div className='d-flex flex-grow-1 text-wrap overflow-hidden'>
 
 <ul className="nav nav-tabs bg-secondary settingwidth">
 
 <Link className="nav-link" to="/">
-    <button class="btn btn-secondary menuimageside" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="fa-solid fa-bars"></i></button>
+    <button className="btn btn-secondary menuimageside" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i className="fa-solid fa-bars"></i></button>
 
-<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-  <div class="offcanvas-header">
-  <Sidebar/>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+<div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+
+  <div className="offcanvas-header justify-content-end">
+  <h1>ShopZo</h1>
+  <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+
   </div>
-  
+  <Sidebar/>  
 </div>
   
     </Link>
