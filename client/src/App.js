7b234052ec3a863b8f81d0781beee1fd1cb4c4fc
Ayush Kumar <ayushk.2125@gmail.com>
2023-03-19 23:21:11 +0530

    ayush/navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,19 @@ import {
 } from "react-router-dom";
 import Navbar from './components/navbar';
 import Signin from "./components/signin/signin";
-import Categorynav from "./components/categorynav/categorynav";
-import Crousel from './components/body/crousel';
 import Signup from "./components/signup/signup";
-import Product from './components/body/product';
 import Footer from './components/footer/footer';
-import Work from './components/work/howwork';
+
 function App() {
   return (
     <>
     <Router>
     
     <Routes>
-    <Route element={[<Navbar/>,<Categorynav/>,<Crousel />,<Product/>,<Footer/>] } exact path='/' ></Route>
+    <Route element={[<Navbar/>,<Footer/>] } exact path='/' ></Route>
       <Route element={<Signin/>} exact path='/signin' ></Route>
       <Route element={<Signup/>} exact path='/signup' ></Route>
-      <Route element={<Work/>} exact path='/working'></Route>
+    
       </Routes>
      
     </Router>
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,48 +1,35 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import './navbar.css';
-import coin from '../img/final_coin.png';
-
+import logo from '../img/logo-3.png';
 
 const navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary navbar navbar bg-dark">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/working">ShopzO</Link>
-
-    <form className="d-flex" role="search">
-        <input className="form-control me-2" type="Search " placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
-      </form>
-
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  </div>
-  
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/signin">Sign-in / Sign-up</Link>
-          
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Returns & orders</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/"><i className="fa-solid fa-cart-shopping">1</i>cart</Link>
-          
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/"><img src={coin} alt='#' width="30" height="30"></img>200</Link>
-        </li>
-        
-      </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/"><img src={logo} alt="/" width={160}></img></Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-5">
+          <li className="nav-item mx-3">
+            <Link className="nav-link active " aria-current="page" to="/">your profile</Link>
+          </li><li className="nav-item mx-3">
+            <Link className="nav-link active" aria-current="page" to="/">Quick notes</Link>
+          </li><li className="nav-item mx-3">
+            <Link className="nav-link active" aria-current="page" to="/">Testimonials</Link>
+          </li><li className="nav-item mx-3">
+            <Link className="nav-link active" aria-current="page" to="/">events</Link>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <button className="btn btn-outline-warning mx-2 " type="submit">Login</button>
+          <button className="btn btn-outline-warning mx-2" type="submit">Register now</button>
+        </form>
+      </div>
     </div>
-</nav>
+  </nav>
   )
 }
 
