ca212932e5836ac64e5b663741e7c638cd7fbf8c
Ayush Kumar <ayushk.2125@gmail.com>
2023-03-20 02:03:31 +0530

    ayush/cards

--- client/src/components/body/cards.js
+++ client/src/components/body/cards.js
@@ -1,35 +1,29 @@
 import React from 'react'
 import './cards.css'
+import teacher from '../../img/teacher.jpg';
+import student from '../../img/student.avif';
 const cards = () => {
   return (
-    <>
-
-
-
-<div className="container">
-  <div className="text-center">
-  </div>
-  <div className="container">
-    <div className="card-columns">
-      <div className="card">
-        <a href="#">
-        <img className="card-img-top" src="https://images.unsplash.com/photo-1535025639604-9a804c092faa?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6cb0ceb620f241feb2f859e273634393&auto=format&fit=crop&w=500&q=80" alt="Card image cap"></img>
-        <div className="card-body">
-          <h5 className="card-title">Student</h5>
-          <p className="card-text">
-            Access to study Material<hr></hr>One to one Interaction<hr></hr>Maintain your profile
-           </p>
-          
-      </div>
-        </a>
-      </div>
-      
+    <div className='d-flex container'>
+    <div className="card mb-3 mx-5" width='18rem'>
+    <img src={teacher} className="card-img-top" alt="..." />
+    <div className="card-body mx-5">
+    <h5> Maintain your profile</h5>
+              <h5> Take classes</h5>
+              <h5> Create Assignments</h5>
+              <h5> Mentor students</h5>
     </div>
   </div>
-</div>
-  
-  </>
-
+  <div className="card mb-3 mx-5" width='18rem'>
+    <img src={student} className="card-img-top" alt="..." />
+    <div className="card-body mx-5">
+    <h5> Maintain your profile</h5>
+              <h5> Easy access to study Material</h5>
+              <h5> One-to-one interaction</h5>
+              <h5> Track your Progress</h5>
+    </div>
+  </div>
+  </div>
   )
 }
 
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -8,7 +8,7 @@ const navbar = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
       <Link className="navbar-brand" to="/"><img src={logo} alt="/" width={160}></img></Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
