18818bb54df1871531db2a2aba6b281430fd3801
Garv2022 <garv7183@gmail.com>
2023-03-27 02:09:41 +0530

    Create privacyPolicy.js

---
+++ client/src/components/PrivacyPolicy/privacyPolicy.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const privacyPolicy = () => {
+  return (
+    <div className='privacy text-white'>
+        <div className="Terms text-white">
+           <h3 className='text-white'>Terms and Conditions:</h3>
+        </div>
+        <div>
+      <div className="Terms text-white">
+        <p className='text-white'>Terms of Service:</p>
+
+        <p className='text-white'>Introduction</p>
+        <p className='text-white'>The following terms and conditions govern your use of our website, Pendown, and all of its content, services, and products. By using our website, you accept and agree to these terms and conditions in full. If you disagree with any part of these terms and conditions, do not use our website.</p>
+
+        <p className='text-white'>Intellectual Property</p>
+        <p className='text-white'>All content on our website, including but not limited to text, graphics, images, logos, and code, is the property of Pendown and is protected by international copyright laws. You may not reproduce, modify, distribute, or otherwise use any content from our website without our prior written consent.</p>
+
+        <p className='text-white'>User Conduct</p>
+        <p className='text-white'>By using our website, you agree to comply with all applicable laws and regulations. You must not use our website in any way that causes, or may cause, damage to the website or impairment of the availability or accessibility of the website; or in any way which is unlawful, illegal, fraudulent or harmful.</p>
+
+        <p className='text-white'>Limitations of Liability</p>
+        <p className='text-white'>Pendown shall not be liable for any damages arising out of or in connection with the use or inability to use our website, including but not limited to direct, indirect, incidental, special, or consequential damages. We do not guarantee the accuracy or completeness of any information on our website, and we reserve the right to make changes to our website at any time without notice.</p>
+
+        <p className='text-white'>Indemnification</p>
+        <p className='text-white'>You agree to indemnify and hold Pendown harmless from any and all claims, damages, expenses, and liabilities, including reasonable attorneys' fees, arising out of or in connection with your use of our website or your breach of these terms and conditions.</p>
+
+        <p className='text-white'>Termination</p>
+        <p className='text-white'>Pendown may terminate or suspend your access to our website at any time, without prior notice or liability, for any reason whatsoever, including without limitation if you breach these terms and conditions.</p>
+
+        <p className='text-white'>Governing Law</p>
+        <p className='text-white'>These terms and conditions shall be governed by and construed in accordance with the laws of [state/country], and any disputes arising under or in connection with these terms and conditions shall be subject to the exclusive jurisdiction of the courts of [state/country].</p>
+
+        <h4 className='text-white'>Privacy Policy:</h4>
+
+        <p className='text-white'>Introduction</p>
+        <p className='text-white'>We take your privacy seriously, and this privacy policy explains how we collect, use, and protect your personal information. By using our website, you consent to the collection and use of your personal information in accordance with this policy.</p>
+
+        <p className='text-white'>Personal Information We Collect</p>
+        <p className='text-white'>We may collect personal information such as your name, email address, and other contact details when you subscribe to our newsletter or fill out a contact form on our website.</p>
+
+        <p className='text-white'>Use of Personal Information</p>
+        <p className='text-white'>We may use your personal information to communicate with you, to send you newsletters, promotional offers or other marketing materials, or to respond to your inquiries or requests.</p>
+ 
+        <p className='text-white'>Disclosure of Personal Information</p>
+        <p className='text-white'>We will not disclose your personal information to any third parties, except as required by law or as necessary to provide services to you.</p>
+
+        <p className='text-white'>Security of Personal Information</p>
+        <p className='text-white'> We take reasonable measures to protect your personal information from unauthorized access or disclosure, and we use secure servers and encryption to protect your information.</p>
+
+        <p className='text-white'>Your Rights</p>
+        <p className='text-white'>You have the right to access and correct your personal information, as well as to request that we delete your personal information from our records.</p>
+    
+        <p className='text-white'>Changes to this Privacy Policy</p>
+        <p className='text-white'>We may make changes to this privacy policy from time to time, and any changes will be posted.</p>
+    </div>
+    </div>
+    </div>
+    
+  )
+}
+
+export default privacyPolicy
+
